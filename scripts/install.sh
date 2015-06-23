16158560fb3a9bc06899556517b13387e1b1e73a
Connor Manning <connor@hobu.co>
2015-06-22 21:11:13 -0500

    Set Release mode in install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ mkdir -p ./entwine/build && cd ./entwine/build
 
 cmake -G "Unix Makefiles" \
     -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+    -DCMAKE_BUILD_TYPE=Release ..
 
 make -j4
 make install
