d5e7e2b6e4691b03b4f46e8f745586f0d9700d34
Connor Manning <connor@hobu.co>
2015-08-05 10:23:04 -0500

    Add little-big-int to the build config.

---
+++ entwine/third/bigint/little-big-int.cpp
@@ -0,0 +1,669 @@
+#include "little-big-int.hpp"
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <sstream>
+
+BigUint::BigUint()
+    : m_val()
+{
+    m_val.push_back(0);
+}
+
+BigUint::BigUint(const Block val)
+    : m_val()
+{
+    m_val.push_back(val);
+}
+
+BigUint::BigUint(std::vector<Block> blocks)
+    : m_val(blocks)
+{
+    if (m_val.empty()) m_val.push_back(0);
+}
+
+BigUint::BigUint(const std::string& str)
+    : m_val()
+{
+    m_val.push_back(0);
+    BigUint factor(1);
+    const std::size_t size(str.size());
+
+    for (std::size_t i(size - 8); i < size; i -= 8)
+    {
+        *this += BigUint(std::stoull(str.substr(i, 8)) * factor);
+        factor *= 100000000;
+    }
+
+    if (size % 8)
+    {
+        *this += BigUint(std::stoull(str.substr(0, size % 8)) * factor);
+    }
+}
+
+BigUint::BigUint(const BigUint& other)
+    : m_val(other.m_val)
+{ }
+
+BigUint& BigUint::operator=(const BigUint& other)
+{
+    m_val = other.m_val;
+    return *this;
+}
+
+std::string BigUint::str() const
+{
+    if (trivial())
+    {
+        return std::to_string(m_val.front());
+    }
+    else
+    {
+        // Guess the number of digits with an approximation of log10(*this).
+        std::deque<char> digits(log2(*this) * 1000 / 3322 + 1, 48);
+        BigUint factor(10);
+        BigUint lagged(1);
+
+        std::pair<BigUint, BigUint> current;
+
+        std::size_t i(0);
+
+        do
+        {
+            current = divMod(factor);
+
+            digits.at(i++) = 48 + (current.second / lagged).getSimple();
+
+            lagged = factor;
+            factor *= 10;
+        }
+        while (!current.first.zero() && i < digits.size());
+
+        while (!current.first.zero())
+        {
+            current = divMod(factor);
+
+            digits.push_back(48 + (current.second / lagged).getSimple());
+
+            lagged = factor;
+            factor *= 10;
+        }
+
+        while (digits.back() == 48) digits.pop_back();
+
+        return std::string(digits.rbegin(), digits.rend());
+    }
+}
+
+std::string BigUint::bin() const
+{
+    std::ostringstream stream;
+    const std::size_t size(m_val.size());
+
+    stream << "0b";
+
+    for (std::size_t i(0); i < size; ++i)
+    {
+        stream << std::bitset<bitsPerBlock>(m_val[size - i - 1]);
+    }
+
+    return stream.str();
+}
+
+unsigned long long BigUint::getSimple() const
+{
+    if (m_val.size() == 1)
+    {
+        return m_val[0];
+    }
+    else
+    {
+        throw std::overflow_error("This BigUint is too large to get as long.");
+    }
+}
+
+void BigUint::add(const BigUint& rhs, const Block shift)
+{
+    auto& rhsVal(rhs.val());
+
+    const std::size_t shiftBlocks(shift / bitsPerBlock);
+    const std::size_t shiftBits(shift % bitsPerBlock);
+    const std::size_t shiftBack(shiftBits ? (bitsPerBlock - shiftBits) : 0);
+
+    const std::size_t rhsSize(rhsVal.size());
+    const std::size_t lhsSize(m_val.size());
+
+    bool carry(false);
+    Block rhsCur(0);
+
+    {
+        if (shiftBlocks >= m_val.size()) m_val.resize(shiftBlocks + 1, 0);
+        Block& lhsCur(m_val.at(shiftBlocks));
+
+        rhsCur = rhsVal.front() << shiftBits;
+        lhsCur += rhsCur;
+
+        carry = lhsCur < rhsCur;
+    }
+
+    std::size_t i;
+    for (i = 1; i < rhsSize; ++i)
+    {
+        if (shiftBlocks + i == m_val.size()) m_val.push_back(0);
+        Block& lhsCur(m_val.at(shiftBlocks + i));
+
+        rhsCur =
+            (rhsVal.at(i) << shiftBits) |
+            (shiftBack ? rhsVal[i - 1] >> shiftBack : 0);
+
+        lhsCur += rhsCur + static_cast<Block>(carry);
+        carry = lhsCur < rhsCur || (carry && lhsCur == rhsCur);
+    }
+
+    {
+        rhsCur = (shiftBack ? rhsVal.back() >> shiftBack : 0);
+
+        if (rhsCur || carry)
+        {
+            if (shiftBlocks + i == m_val.size()) m_val.push_back(0);
+
+            Block& lhsCur(m_val[shiftBlocks + i]);
+            lhsCur += rhsCur + static_cast<Block>(carry);
+            carry = lhsCur < rhsCur || (carry && lhsCur == rhsCur);
+            ++i;
+        }
+    }
+
+    while (carry && (shiftBlocks + i < lhsSize))
+    {
+        carry = (++m_val[i] == 0);
+        ++i;
+    }
+
+    if (carry) m_val.push_back(1);
+}
+
+std::pair<BigUint, BigUint> BigUint::divMod(const BigUint& d) const
+{
+    std::pair<BigUint, BigUint> result;
+    auto& q(result.first);
+    auto& r(result.second);
+
+    const auto& dVal(d.val());
+
+    if (d.zero()) throw std::invalid_argument("Cannot divide by zero");
+
+    if (trivial() && d.trivial())
+    {
+        q = m_val.front() / dVal.front();
+        r = m_val.front() % dVal.front();
+        return result;
+    }
+
+    if (*this < d)
+    {
+        q = 0;
+        r = m_val;
+        return result;
+    }
+    else
+    {
+        const std::size_t nValSize(m_val.size());
+
+        // Don't presize the quotient here, we can't have leading zero blocks
+        // since it can cause errors in our other operators.
+        auto& qVal(q.val());
+        Block mask(0);
+
+        for (std::size_t block(nValSize - 1); block < nValSize; --block)
+        {
+            for (std::size_t bit(bitsPerBlock - 1); bit < bitsPerBlock; --bit)
+            {
+                r <<= 1;
+                mask = Block(1) << bit;
+
+                if (m_val.at(block) & mask) r.incSimple();
+
+                if (r >= d)
+                {
+                    r -= d;
+
+                    if (block >= qVal.size()) qVal.resize(block + 1, 0);
+                    qVal.at(block) |= mask;
+                }
+            }
+        }
+
+        while (!qVal.back()) qVal.pop_back();
+    }
+
+    return result;
+}
+
+std::vector<Block>& BigUint::val() { return m_val; }
+const std::vector<Block>& BigUint::val() const { return m_val; }
+
+BigUint& operator+=(BigUint& lhs, const BigUint& rhs)
+{
+    auto& lhsVal(lhs.val());
+    auto& rhsVal(rhs.val());
+
+    auto& lhsFront(lhsVal.front());
+    const Block rhsFront(rhsVal.front());
+
+    if (lhs.trivial() && rhs.trivial() && lhsFront + rhsFront >= lhsFront)
+    {
+        lhsFront += rhsFront;
+    }
+    else
+    {
+        const std::size_t rhsSize(rhsVal.size());
+        lhsVal.resize(std::max(lhsVal.size(), rhsSize), 0);
+        const std::size_t lhsSize(lhsVal.size());
+
+        bool carry(false);
+        std::size_t i(0);
+
+        while (i < rhsSize)
+        {
+            Block& lhsCur(lhsVal[i]);
+            const Block& rhsCur(rhsVal[i]);
+
+            lhsCur += rhsCur + static_cast<Block>(carry);
+            carry = lhsCur < rhsCur || (carry && lhsCur == rhsCur);
+
+            ++i;
+        }
+
+        while (carry && i < lhsSize)
+        {
+            carry = (++lhsVal[i] == 0);
+            ++i;
+        }
+
+        if (carry) lhsVal.push_back(1);
+    }
+
+    return lhs;
+}
+
+BigUint& operator-=(BigUint& lhs, const BigUint& rhs)
+{
+    auto& lhsVal(lhs.val());
+    const auto& rhsVal(rhs.val());
+
+    const std::size_t rhsSize(rhsVal.size());
+    const std::size_t lhsSize(lhsVal.size());
+
+    if (lhsSize < rhsSize)
+    {
+        throw std::underflow_error(
+                "Subtraction result was negative (block size)");
+    }
+    else if (lhs.trivial())
+    {
+        if (lhsVal.front() >= rhsVal.front()) lhsVal.front() -= rhsVal.front();
+        else throw std::underflow_error(
+                "Subtraction result was negative (block zero)");
+    }
+    else
+    {
+        Block old(0);
+        bool borrow(false);
+        std::size_t i(0);
+
+        while (i < rhsSize)
+        {
+            Block& lhsCur(lhsVal[i]);
+            old = lhsCur;
+            const Block& rhsCur(rhsVal[i]);
+
+            lhsCur -= rhsCur + static_cast<Block>(borrow);
+            borrow = lhsCur > old || (borrow && lhsCur == old);
+
+            ++i;
+        }
+
+        while (borrow && i < lhsSize)
+        {
+            borrow = (lhsVal[i] == 0);
+            --lhsVal[i];
+            ++i;
+        }
+
+        if (borrow)
+        {
+            throw std::underflow_error(
+                    "Subtraction result was negative (borrow out)");
+        }
+
+        while (lhsVal.size() != 1 && lhsVal.back() == 0) lhsVal.pop_back();
+    }
+
+    return lhs;
+}
+
+BigUint& operator*=(BigUint& lhs, const BigUint& rhs)
+{
+    if (lhs.zero() || rhs.zero())
+    {
+        lhs = 0;
+    }
+    else if (log2(lhs) + log2(rhs) + 1 <= bitsPerBlock)
+    {
+        lhs = lhs.val().front() * rhs.val().front();
+    }
+    else
+    {
+        BigUint out;
+        auto& rhsVal(rhs.val());
+
+        for (std::size_t block(0); block < rhsVal.size(); ++block)
+        {
+            for (std::size_t bit(0); bit < bitsPerBlock; ++bit)
+            {
+                if ((rhsVal.at(block) >> bit) & 1)
+                {
+                    out.add(lhs, block * bitsPerBlock + bit);
+                }
+            }
+        }
+
+        lhs = out;
+    }
+
+    return lhs;
+}
+
+BigUint& operator/=(BigUint& n, const BigUint& d)
+{
+    const auto div(n.divMod(d));
+    n = div.first;
+    return n;
+}
+
+BigUint& operator%=(BigUint& n, const BigUint& d)
+{
+    const auto div(n.divMod(d));
+    n = div.second;
+    return n;
+}
+
+BigUint& operator&=(BigUint& lhs, const BigUint& rhs)
+{
+    auto& lhsVal(lhs.val());
+    auto& rhsVal(rhs.val());
+
+    lhsVal.resize(std::min(lhsVal.size(), rhsVal.size()));
+
+    for (std::size_t i(0); i < lhsVal.size(); ++i)
+    {
+        lhsVal[i] &= rhsVal[i];
+    }
+
+    while (!lhs.zero() && !lhsVal.back()) lhsVal.pop_back();
+
+    return lhs;
+}
+
+BigUint& operator|=(BigUint& lhs, const BigUint& rhs)
+{
+    auto& lhsVal(lhs.val());
+    auto& rhsVal(rhs.val());
+
+    const std::size_t rhsSize(rhsVal.size());
+
+    lhsVal.resize(std::max(lhsVal.size(), rhsSize), 0);
+
+    for (std::size_t i(0); i < rhsSize; ++i)
+    {
+        lhsVal[i] |= rhsVal[i];
+    }
+
+    return lhs;
+}
+
+BigUint& operator<<=(BigUint& lhs, Block rhs)
+{
+    const std::size_t startBlocks(lhs.blockSize());
+    const std::size_t shiftBlocks(rhs / bitsPerBlock);
+    const std::size_t shiftBits(rhs % bitsPerBlock);
+    const std::size_t shiftBack(shiftBits ? (bitsPerBlock - shiftBits) : 0);
+
+    auto& val(lhs.val());
+
+    {
+        Block carry(shiftBack ? val.back() >> shiftBack : 0);
+        val.resize(startBlocks + shiftBlocks + (carry ? 1 : 0), 0);
+        if (carry) val.back() = carry;
+    }
+
+    for (std::size_t i(startBlocks - 1); i < startBlocks; --i)
+    {
+        val[i + shiftBlocks] =
+            (shiftBack && i ? val[i - 1] >> shiftBack : 0) |
+            (val[i] << shiftBits);
+    }
+
+    for (std::size_t i(0); i < shiftBlocks; ++i)
+    {
+        val[i] = 0;
+    }
+
+    return lhs;
+}
+
+BigUint& operator>>=(BigUint& lhs, Block rhs)
+{
+    const std::size_t startBlocks(lhs.blockSize());
+    const std::size_t shiftBlocks(rhs / bitsPerBlock);
+    const std::size_t shiftBits(rhs % bitsPerBlock);
+    const std::size_t shiftBack(shiftBits ? (bitsPerBlock - shiftBits) : 0);
+
+    auto& val(lhs.val());
+
+    for (std::size_t i(shiftBlocks); i < startBlocks - 1; ++i)
+    {
+        val[i - shiftBlocks] =
+            (shiftBack ? val[i + 1] << shiftBack : 0) |
+            (val[i] >> shiftBits);
+    }
+
+    Block last(val.back() >> shiftBits);
+    val.resize(startBlocks - shiftBlocks - (last ? 0 : 1));
+    if (last) val.back() = last;
+
+    return lhs;
+}
+
+BigUint operator+(const BigUint& lhs, const BigUint& rhs)
+{
+    BigUint result(lhs);
+    result += rhs;
+    return result;
+}
+
+BigUint operator-(const BigUint& lhs, const BigUint& rhs)
+{
+    BigUint result(lhs);
+    result -= rhs;
+    return result;
+}
+
+BigUint operator*(const BigUint& lhs, const BigUint& rhs)
+{
+    BigUint result(lhs);
+    result *= rhs;
+    return result;
+}
+
+BigUint operator/(const BigUint& lhs, const BigUint& rhs)
+{
+    BigUint result(lhs);
+    result /= rhs;
+    return result;
+}
+
+BigUint operator%(const BigUint& lhs, const BigUint& rhs)
+{
+    BigUint result(lhs);
+    result %= rhs;
+    return result;
+}
+
+BigUint operator&(const BigUint& lhs, const BigUint& rhs)
+{
+    BigUint result;
+    if (lhs.val().size() < rhs.val().size())
+    {
+        result = lhs;
+        result &= rhs;
+    }
+    else
+    {
+        result = rhs;
+        result &= lhs;
+    }
+    return result;
+}
+
+BigUint operator|(const BigUint& lhs, const BigUint& rhs)
+{
+    BigUint result(lhs);
+    result |= rhs;
+    return result;
+}
+
+BigUint operator<<(const BigUint& lhs, const Block rhs)
+{
+    const std::size_t startBlocks(lhs.blockSize());
+    const std::size_t shiftBlocks(rhs / bitsPerBlock);
+    const std::size_t shiftBits(rhs % bitsPerBlock);
+
+    BigUint result(std::vector<Block>(startBlocks + shiftBlocks, 0));
+
+    const auto& start(lhs.val());
+    auto& val(result.val());
+
+    Block carry(0);
+
+    for (std::size_t i(0); i < startBlocks; ++i)
+    {
+        val[i + shiftBlocks] = (start[i] << shiftBits) | carry;
+        carry = shiftBits ? (start[i] >> (bitsPerBlock - shiftBits)) : 0;
+    }
+
+    if (carry) val.push_back(carry);
+
+    return result;
+}
+
+BigUint operator>>(const BigUint& lhs, const Block rhs)
+{
+    // TODO Can be optimized like the above.
+    BigUint result(lhs);
+    result >>= rhs;
+    return result;
+}
+
+BigUint& operator--(BigUint& lhs)
+{
+    lhs -= 1;
+    return lhs;
+}
+
+BigUint operator--(BigUint& lhs, int)
+{
+    BigUint copy(lhs);
+    lhs -= 1;
+    return copy;
+}
+
+BigUint& operator++(BigUint& lhs)
+{
+    lhs += 1;
+    return lhs;
+}
+
+BigUint operator++(BigUint& lhs, int)
+{
+    BigUint copy(lhs);
+    lhs += 1;
+    return copy;
+}
+
+bool operator==(const BigUint& lhs, const BigUint& rhs)
+{
+    const auto& lhsVal(lhs.val());
+    const auto& rhsVal(rhs.val());
+
+    const std::size_t size(lhsVal.size());
+
+    if (size != rhsVal.size()) return false;
+
+    for (std::size_t i(0); i < size; ++i)
+    {
+        if (lhsVal[i] != rhsVal[i]) return false;
+    }
+
+    return true;
+}
+
+bool operator!=(const BigUint& lhs, const BigUint& rhs)
+{
+    return !(lhs == rhs);
+}
+
+bool operator<(const BigUint& lhs, const BigUint& rhs)
+{
+    const auto& lhsVal(lhs.val());
+    const auto& rhsVal(rhs.val());
+
+    const std::size_t lhsSize(lhsVal.size());
+    const std::size_t rhsSize(rhsVal.size());
+
+    if      (lhsSize < rhsSize) return true;
+    else if (lhsSize > rhsSize) return false;
+    else
+    {
+        for (std::size_t i(lhsSize - 1); i < lhsSize; --i)
+        {
+            if      (lhsVal[i] < rhsVal[i]) return true;
+            else if (lhsVal[i] > rhsVal[i]) return false;
+        }
+
+        return false;
+    }
+}
+
+bool operator<=(const BigUint& lhs, const BigUint& rhs)
+{
+    return !(rhs < lhs);
+}
+
+bool operator>(const BigUint& lhs, const BigUint& rhs)
+{
+    return rhs < lhs;
+}
+
+bool operator>=(const BigUint& lhs, const BigUint& rhs)
+{
+    return !(lhs < rhs);
+}
+
+std::ostream& operator<<(std::ostream& out, const BigUint& val)
+{
+    out << val.str();
+    return out;
+}
+
+Block log2(const BigUint& in)
+{
+    if (in.zero()) throw std::runtime_error("log2(0) is undefined");
+    return std::log2(in.val().back()) + (in.blockSize() - 1) * bitsPerBlock;
+}
+
---
+++ entwine/third/bigint/little-big-int.hpp
@@ -0,0 +1,159 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+#include <ostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+typedef unsigned long long Block;
+static const std::size_t bitsPerBlock(CHAR_BIT * sizeof(Block));
+static const std::size_t blockMax(std::numeric_limits<Block>::max());
+
+class BigUint
+{
+public:
+    BigUint();
+    BigUint(unsigned long long val);
+    explicit BigUint(const std::string& val);
+
+    BigUint(const BigUint& other);
+    BigUint& operator=(const BigUint& other);
+
+    // True if this object represents zero.
+    bool zero() const { return trivial() && !m_val.front(); }
+
+    // True if this object is of size one, so simple integer math may be used
+    // for some operations.
+    bool trivial() const { return m_val.size() == 1; }
+    std::size_t blockSize() const { return m_val.size(); }
+
+    std::string str() const;    // Get base-10 representation.
+    std::string bin() const;    // Get binary representation.
+
+    // Get as an unsigned long long.  Throws std::overflow_error if !trivial().
+    unsigned long long getSimple() const;
+
+    // If both the quotient and remainder are required, using the result of
+    // this function is more efficient than calling both / and % operators
+    // separately.
+    //
+    // Return value:
+    //      result.first - quotient
+    //      result.second - remainder
+    std::pair<BigUint, BigUint> divMod(const BigUint& denominator) const;
+
+    // Increments the least-significant block without carrying any possible
+    // roll-over into subsequent blocks.  Can be used to increment efficiently
+    // only if the caller can guarantee that no carry will be produced by this
+    // increment.  For example, after a positive bit-shift left.
+    void incSimple() { ++m_val.front(); }
+
+    // Get raw blocks.  For the non-const version, if the result is modified,
+    // all future operations may be incorrect.
+    std::vector<Block>& val();
+    const std::vector<Block>& val() const;
+
+    // These ones need access to private members.
+    friend BigUint& operator*=(BigUint& lhs, const BigUint& rhs);
+    friend BigUint operator<<(const BigUint&, Block);
+
+private:
+    BigUint(std::vector<Block> blocks);
+
+    // Equivalent to:
+    //      *this += (other << shift)
+    //
+    // ...without the copy overhead of performing the shift in advance.
+    void add(const BigUint& other, Block shift);
+
+    std::vector<Block> m_val;
+};
+
+// Assignment.
+BigUint& operator+=(BigUint& lhs, const BigUint& rhs);
+BigUint& operator-=(BigUint& lhs, const BigUint& rhs);
+BigUint& operator*=(BigUint& lhs, const BigUint& rhs);
+BigUint& operator/=(BigUint& lhs, const BigUint& rhs);
+BigUint& operator%=(BigUint& lhs, const BigUint& rhs);
+
+BigUint& operator&=(BigUint& lhs, const BigUint& rhs);
+BigUint& operator|=(BigUint& lhs, const BigUint& rhs);
+BigUint& operator<<=(BigUint& lhs, Block rhs);
+BigUint& operator>>=(BigUint& lhs, Block rhs);
+
+// Copying.
+BigUint operator+(const BigUint& lhs, const BigUint& rhs);
+BigUint operator-(const BigUint& lhs, const BigUint& rhs);
+BigUint operator*(const BigUint& lhs, const BigUint& rhs);
+BigUint operator/(const BigUint& lhs, const BigUint& rhs);
+BigUint operator%(const BigUint& lhs, const BigUint& rhs);
+
+BigUint operator&(const BigUint& lhs, const BigUint& rhs);
+BigUint operator|(const BigUint& lhs, const BigUint& rhs);
+BigUint operator<<(const BigUint& lhs, Block rhs);
+BigUint operator>>(const BigUint& lhs, Block rhs);
+
+// Pre/post-fixes.  These return *this or a copy, as expected.
+BigUint& operator--(BigUint& lhs);      // Pre-decrement.
+BigUint  operator--(BigUint& lhs, int); // Post-decrement.
+BigUint& operator++(BigUint& lhs);      // Pre-increment.
+BigUint  operator++(BigUint& lhs, int); // Post-increment.
+
+// Comparisons.
+bool operator==(const BigUint& lhs, const BigUint& rhs);
+bool operator!=(const BigUint& lhs, const BigUint& rhs);
+bool operator< (const BigUint& lhs, const BigUint& rhs);
+bool operator<=(const BigUint& lhs, const BigUint& rhs);
+bool operator> (const BigUint& lhs, const BigUint& rhs);
+bool operator>=(const BigUint& lhs, const BigUint& rhs);
+
+std::ostream& operator<<(std::ostream& out, const BigUint& val);
+
+Block log2(const BigUint& val);
+
+namespace std
+{
+
+template<> struct hash<BigUint>
+{
+    // Based on the public domain Murmur hash, by Austin Appleby.
+    // https://sites.google.com/site/murmurhash/
+    std::size_t operator()(const BigUint& big) const
+    {
+        const Block seed(0xc70f6907ULL);
+        const Block m(0xc6a4a7935bd1e995ULL);
+        const Block r(47);
+
+        const auto& val(big.val());
+
+        Block h(seed ^ (val.size() * sizeof(Block) * m));
+
+        const Block* cur(val.data());
+        const Block* end(val.data() + val.size());
+
+        Block k(0);
+
+        while (cur != end)
+        {
+            k = *cur++;
+
+            k *= m;
+            k ^= k >> r;
+            k *= m;
+
+            h ^= k;
+            h *= m;
+        }
+
+        h ^= h >> r;
+        h *= m;
+        h ^= h >> r;
+
+        return h;
+    }
+};
+
+}
+
