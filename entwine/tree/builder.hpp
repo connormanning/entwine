a27492ea5385bf6629e3270d86404f4fc024679d
Connor Manning <connor@hobu.co>
2015-12-09 13:03:45 -0600

    Add immediate termination call, and allow merger to merge into an uncontinuable aggregate.

--- entwine/tree/builder.hpp
+++ entwine/tree/builder.hpp
@@ -114,6 +114,7 @@ public:
     const arbiter::Endpoint& tmpEndpoint() const { return *m_tmpEndpoint; }
 
     bool setEnd(Origin end);
+    void stop();
 
 private:
     // Returns true if we should insert this file.
