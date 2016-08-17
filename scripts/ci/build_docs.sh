1b181bf25a9a50cd92faa024c03b6b4def51b207
Howard Butler <howard@hobu.co>
2016-08-17 16:47:20 -0500

    doc path typo

--- scripts/ci/build_docs.sh
+++ scripts/ci/build_docs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "building docs for $TRAVIS_BUILD_DIR/docs"
+echo "building docs for $TRAVIS_BUILD_DIR/doc"
 buildpath=`pwd`
 if [[ ! -z $TRAVIS_BUILD_DIR ]]; then
 buildpath="$TRAVIS_BUILD_DIR"
 fi
 
-docker run -v $buildpath:/data -w /data/docs hobu/entwine-docs make html
+docker run -v $buildpath:/data -w /data/doc hobu/entwine-docs make html
 
 
