aeaef5422a96d70b072238073290b1cd7eadcdd3
Connor Manning <connor@hobu.co>
2016-12-05 11:50:33 -0600

    Use latest PDAL for Travis.

--- scripts/ci/docker.sh
+++ scripts/ci/docker.sh
@@ -3,6 +3,6 @@
 # pdal/doc image has all of the Sphinx
 # dependencies need to build PDAL's docs
 
-docker pull connormanning/pdal:entwine-pin
+docker pull pdal/pdal:latest
 docker pull hobu/entwine-docs
 
