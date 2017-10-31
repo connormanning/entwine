8c84877892a12451e3e03bfe294fa8c27c548782
Howard Butler <howard@hobu.co>
2017-10-31 14:39:54 -0500

    use nmake for building on appveyor

--- scripts/appveyor/config.cmd
+++ scripts/appveyor/config.cmd
@@ -1,6 +1,7 @@
 @echo off
 
-cmake -Wno-dev -G "Visual Studio 14 2015 Win64" ^
-    -DCMAKE_INSTALL_PREFIX=C:\pdalbin 	^
+cmake -Wno-dev -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=C:/OSGeo4W64 ^
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
     -DBUILD_SHARED_LIBS=ON ^
     -Dgtest_force_shared_crt=ON .
