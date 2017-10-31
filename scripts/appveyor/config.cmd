82608f69d10b1dc88b43713085de84e63b25bd4e
Howard Butler <howard@hobu.co>
2017-10-30 20:36:29 -0500

    more windows noise

--- scripts/appveyor/config.cmd
+++ scripts/appveyor/config.cmd
@@ -1,3 +1,6 @@
 @echo off
 
-cmake -Wno-dev -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=C:\pdalbin .
+cmake -Wno-dev -G "Visual Studio 14 2015 Win64" ^
+    -DCMAKE_INSTALL_PREFIX=C:\pdalbin 	^
+    -DBUILD_SHARED_LIBS=ON ^
+    -Dgtest_force_shared_crt=ON .
