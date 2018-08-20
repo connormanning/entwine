8d7a3922c7e26a520702ee1c8ac2310a2d8b6a75
Howard Butler <howard@hobu.co>
2018-08-20 15:12:41 -0500

    remove debug lint

--- test/unit/build.cpp
+++ test/unit/build.cpp
@@ -21,10 +21,8 @@ TEST(build, basic)
     c["output"] = out;
     c["force"] = true;
     c["ticks"] = static_cast<Json::UInt64>(v.ticks());
-    
     c["hierarchyStep"] = static_cast<Json::UInt64>(v.hierarchyStep());
 
-    std::cout << c << std::endl;
     Builder b(c);
     b.go();
 
