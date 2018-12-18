0bab8a903e3833f4b068c85bfd5cc8f5060deafa
Connor Manning <connor@hobu.co>
2018-12-18 13:36:37 -0600

    Remove 'update' app.  Bump EPT version.

--- app/entwine.cpp
+++ app/entwine.cpp
@@ -13,7 +13,6 @@
 #include "convert.hpp"
 #include "merge.hpp"
 #include "scan.hpp"
-#include "update.hpp"
 
 #include <csignal>
 #include <cstdio>
@@ -44,9 +43,7 @@ namespace
             t(2) + "merge\n" +
             t(3) + "Merge colocated entwine subsets\n" +
             t(2) + "convert\n" +
-            t(3) + "Convert an entwine dataset to a different format\n" +
-            t(2) + "update\n" +
-            t(3) + "Update a development EPT dataset to current EPT\n";
+            t(3) + "Convert an entwine dataset to a different format\n";
     }
 
     std::mutex mutex;
@@ -320,10 +317,6 @@ int main(int argc, char** argv)
         else if (app == "convert")
         {
             entwine::app::Convert().go(args);
-        }
-        else if (app == "update")
-        {
-            entwine::app::Update().go(args);
         }
         else
         {
