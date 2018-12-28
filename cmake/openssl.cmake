e6fe48f95e854005f359a2db7e5db1ef8f33a129
Andrew Bell <andrew.bell.ia@gmail.com>
2018-12-28 15:43:58 -0600

    Build updates.

---
+++ cmake/openssl.cmake
@@ -0,0 +1,10 @@
+find_package(OpenSSL 1.0.1)
+if (OPENSSL_FOUND)
+    set(ARBITER_OPENSSL TRUE)
+    set(ENTWINE_OPENSSL TRUE)
+	set(OPENSSL_DEFS ARBITER_OPENSSL ENTWINE_OPENSSL)
+else()
+    # For me this is /usr/local/opt/openssl\@1.1
+    message("OpenSSL NOT found - `export OPENSSL_ROOT_DIR=___`")
+    message("Google storage IO will not be available")
+endif()
