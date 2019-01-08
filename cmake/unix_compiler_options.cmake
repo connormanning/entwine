cb17ec49165b9cbef4a7b88366b9cd7aa84bafa3
nicolas-chaulet <nicolas.chaulet@gmail.com>
2019-01-08 12:47:35 +0000

    Fix build for Clang compiler

--- cmake/unix_compiler_options.cmake
+++ cmake/unix_compiler_options.cmake
@@ -1,10 +1,20 @@
 function(system_compiler_options target)
-    target_compile_options(${target}
-        PRIVATE
-            -Wno-deprecated-declarations
-            -Wall
-            -pedantic
-            -fexceptions
-            -Werror
-    )
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        target_compile_options(${target}
+            PRIVATE
+                -Wno-deprecated-declarations
+                -Wall
+                -pedantic
+                -fexceptions
+        )
+    else()
+        target_compile_options(${target}
+            PRIVATE
+                -Wno-deprecated-declarations
+                -Wall
+                -pedantic
+                -fexceptions
+                -Werror
+        )
+    endif()
 endfunction()
