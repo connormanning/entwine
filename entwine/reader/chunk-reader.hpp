connormanning/entwine

commit cae1e4a79e579624ed477309ac9d2c13463b465d

Clean up DimInfo ctors flow.  Update reader to contiguous model, re-enable tests.

entwine/reader/chunk-reader.hpp
@@ -13,6 +13,7 @@
 #include <memory>
 
 #include <entwine/types/key.hpp>
+#include <entwine/types/vector-point-table.hpp>
 
 namespace entwine
 {
@@ -23,18 +24,12 @@ class ChunkReader
 {
 public:
     ChunkReader(const Reader& reader, const Dxyz& id);
-    ~ChunkReader()
-    {
-        m_pointPool.release(std::move(m_cells));
-    }
 
-    const Cell::PooledStack& cells() const { return m_cells; }
-    uint64_t pointSize() const { return m_pointSize; }
+    VectorPointTable& table() { return m_table; }
+    std::size_t bytes() const { return m_table.size() + m_table.pointSize(); }
 
 private:
-    PointPool m_pointPool;
-    Cell::PooledStack m_cells;
-    uint64_t m_pointSize;
+    VectorPointTable m_table;
 };
 
 using SharedChunkReader = std::shared_ptr<ChunkReader>;

CMakeLists.txt
@@ -123,7 +123,7 @@ add_subdirectory(app)
 
 set(OBJS
     $<TARGET_OBJECTS:formats>
-    # $<TARGET_OBJECTS:reader>
+    $<TARGET_OBJECTS:reader>
     $<TARGET_OBJECTS:io>
     $<TARGET_OBJECTS:third>
     $<TARGET_OBJECTS:builder>
@@ -178,9 +178,9 @@ install(
     FILES "${PROJECT_BINARY_DIR}/entwine-config.cmake"
     DESTINATION lib/cmake/entwine)
 
-# add_subdirectory(test/gtest-1.8.0)
-# include_directories(entwine test/gtest-1.8.0/include test/gtest-1.8.0)
-# add_subdirectory(test)
+add_subdirectory(test/gtest-1.8.0)
+include_directories(entwine test/gtest-1.8.0/include test/gtest-1.8.0)
+add_subdirectory(test)
 
 include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/cpack.cmake")
 add_custom_target(dist COMMAND ${CMAKE_MAKE_PROGRAM} package_source)

entwine/CMakeLists.txt
@@ -1,6 +1,6 @@
 add_subdirectory(builder)
 add_subdirectory(io)
-# add_subdirectory(reader)
+add_subdirectory(reader)
 add_subdirectory(third)
 add_subdirectory(types)
 add_subdirectory(util)

entwine/builder/scan.cpp
@@ -144,13 +144,7 @@ void Scan::add(FileInfo& f, const std::string localPath)
         DimList dims;
         for (const std::string name : preview->dimNames)
         {
-            const DimId id(pdal::Dimension::id(name));
-            DimType t(DimType::Double);
-
-            try { t = pdal::Dimension::defaultType(id); }
-            catch (pdal::pdal_error&) { }
-
-            dims.emplace_back(name, t, id);
+            dims.emplace_back(name);
         }
 
         const Scale scale(preview->scale ? *preview->scale : 1);

entwine/reader/cache.cpp
@@ -47,8 +47,7 @@ SharedChunkReader Cache::get(const Reader& reader, const Dxyz& key)
 
         ChunkReaderInfo& info(it->second);
         info.chunk = std::make_shared<ChunkReader>(reader, key);
-        m_size += info.chunk->cells().size() *
-            reader.metadata().schema().pointSize();
+        m_size += info.chunk->bytes();
     }
     else
     {
@@ -74,7 +73,7 @@ void Cache::purge()
         const ChunkReaderInfo& info(it->second);
 
         std::cout << "\tDel " << id.key << std::endl;
-        m_size -= info.chunk->cells().size() * info.chunk->pointSize();
+        m_size -= info.chunk->bytes();
         m_order.pop_back();
         m_chunks.erase(it);
     }

entwine/reader/chunk-reader.cpp
@@ -17,14 +17,21 @@ namespace entwine
 {
 
 ChunkReader::ChunkReader(const Reader& r, const Dxyz& id)
-    : m_pointPool(r.metadata().schema(), 1024)
-    , m_cells(r.metadata().dataIo().read(
-                r.ep(),
-                r.tmp(),
-                m_pointPool,
-                id.toString()))
-    , m_pointSize(r.metadata().schema().pointSize())
-{ }
+    : m_table(r.metadata().schema(), 0)
+{
+    std::vector<char> data;
+    VectorPointTable tmp(r.metadata().schema());
+    tmp.setProcess([&data, &tmp]()
+    {
+        data.insert(
+                data.end(),
+                tmp.data().data(),
+                tmp.data().data() + tmp.size() * tmp.pointSize());
+    });
+    r.metadata().dataIo().read(r.ep(), r.tmp(), id.toString(), tmp);
+
+    m_table.assign(std::move(data));
+}
 
 } // namespace entwine
 

entwine/reader/query.cpp
@@ -21,8 +21,6 @@ Query::Query(const Reader& r, const Json::Value& j)
     , m_hierarchy(r.hierarchy())
     , m_params(j)
     , m_filter(m_metadata, m_params)
-    , m_table(m_metadata.schema())
-    , m_pointRef(m_table, 0)
     , m_overlaps(overlaps())
 { }
 
@@ -63,24 +61,26 @@ void Query::run()
 
         for (auto& chunk : block)
         {
-            for (const auto& cell : chunk->cells())
+            for (const auto& pr: chunk->table())
             {
-                maybeProcess(cell);
+                maybeProcess(pr);
             }
         }
     }
 }
 
-void Query::maybeProcess(const Cell& cell)
+void Query::maybeProcess(const pdal::PointRef& pr)
 {
-    if (!m_params.bounds().contains(cell.point())) return;
-    m_table.setPoint(cell.uniqueData());
-    if (!m_filter.check(m_pointRef)) return;
-    process(cell);
+    const Point point(
+            pr.getFieldAs<double>(DimId::X),
+            pr.getFieldAs<double>(DimId::Y),
+            pr.getFieldAs<double>(DimId::Z));
+    if (!m_params.bounds().contains(point) || !m_filter.check(pr)) return;
+    process(pr);
     ++m_numPoints;
 }
 
-void ReadQuery::process(const Cell& cell)
+void ReadQuery::process(const pdal::PointRef& pr)
 {
     m_data.resize(m_data.size() + m_schema.pointSize(), 0);
     char* pos(m_data.data() + m_data.size() - m_schema.pointSize());
@@ -95,7 +95,7 @@ void ReadQuery::process(const Cell& cell)
         if (dimNum < 3 && m_delta.exists())
         {
             d = Point::scale(
-                    m_pointRef.getFieldAs<double>(dimInfo.id()),
+                    pr.getFieldAs<double>(dimInfo.id()),
                     m_delta.scale()[dimNum],
                     m_delta.offset()[dimNum]);
 
@@ -103,7 +103,7 @@ void ReadQuery::process(const Cell& cell)
         }
         else
         {
-            m_pointRef.getField(pos, dimInfo.id(), dimInfo.type());
+            pr.getField(pos, dimInfo.id(), dimInfo.type());
         }
 
         pos += dimInfo.size();

entwine/reader/query.hpp
@@ -35,7 +35,7 @@ public:
     uint64_t numPoints() const { return m_numPoints; }
 
 protected:
-    virtual void process(const Cell& cell) { }
+    virtual void process(const pdal::PointRef& pr) { }
 
     const Reader& m_reader;
     const Metadata& m_metadata;
@@ -43,14 +43,11 @@ protected:
     const QueryParams m_params;
     const Filter m_filter;
 
-    BinaryPointTable m_table;
-    pdal::PointRef m_pointRef;
-
 private:
     HierarchyReader::Keys overlaps() const;
     void overlaps(HierarchyReader::Keys& keys, const ChunkKey& c) const;
 
-    void maybeProcess(const Cell& cell);
+    void maybeProcess(const pdal::PointRef& pr);
 
     HierarchyReader::Keys m_overlaps;
     uint64_t m_numPoints = 0;
@@ -79,7 +76,7 @@ public:
     const std::vector<char>& data() const { return m_data; }
 
 protected:
-    virtual void process(const Cell& cell) override;
+    virtual void process(const pdal::PointRef& pr) override;
 
 private:
     void setAs(char* dst, double d, pdal::Dimension::Type t)

commit f2a58aa49c1c79fee406f0db06e873ff09ab6dbb

ChunkReader calculates maximum chunk size correctly.
Fixes #169

The bug was introduced in commit cae1e4a79e579624ed477309ac9d2c13463b465d

entwine/reader/chunk-reader.hpp
@@ -29,7 +29,7 @@ public:
     VectorPointTable& table() { return *m_table; }
     std::size_t bytes() const
     {
-        return m_table->capacity() + m_table->pointSize();
+        return m_table->capacity() * m_table->pointSize();
     }
 
 private: