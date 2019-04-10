993ff90098633dd2f7b9c93158869e32508cb8b8
Connor Manning <connor@hobu.co>
2019-04-09 23:01:03 -0500

    Remove zstd pre-sort for now, back compression level off a bit.

--- entwine/io/zstandard.cpp
+++ entwine/io/zstandard.cpp
@@ -23,6 +23,9 @@ void Zstandard::write(
         const Bounds& bounds,
         BlockPointTable& src) const
 {
+    const std::vector<char> uncompressed(pack(src));
+
+    /*
     const Schema& outSchema(m_metadata.outSchema());
     VectorPointTable table(outSchema, pack(src));
 
@@ -38,17 +41,21 @@ void Zstandard::write(
                     return a.compare(DimId::GpsTime, b);
                 });
     }
+    */
 
     std::vector<char> compressed;
     pdal::ZstdCompressor compressor([&compressed](char* pos, std::size_t size)
     {
         compressed.insert(compressed.end(), pos, pos + size);
-    }, 8); // ZSTD_CLEVEL_DEFAULT = 3.
+    }, 3); // ZSTD_CLEVEL_DEFAULT = 3.
 
+    /*
     for (std::size_t i(0); i < view.size(); ++i)
     {
         compressor.compress(view.getPoint(i), outSchema.pointSize());
     }
+    */
+    compressor.compress(uncompressed.data(), uncompressed.size());
     compressor.done();
 
     ensurePut(out, filename + ".zst", compressed);
