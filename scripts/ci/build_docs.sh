0fd79e5f43f73e8698ae13fce75ef6999b5da9cf
Howard Butler <howard@hobu.co>
2020-09-28 13:42:21 -0500

    more failures

--- scripts/ci/build_docs.sh
+++ scripts/ci/build_docs.sh
@@ -9,5 +9,7 @@ fi
 # osgeo/proj-docs contains everything to build the website and
 # it is kept up to date by the PROJ team
 docker run -v $buildpath:/data -w /data/doc osgeo/proj-docs make html
+docker run -v $buildpath:/data -w /data/doc osgeo/proj-docs make latexpdf
+docker run -v $buildpath:/data -w /data/doc osgeo/proj-docs make spelling
 
 
