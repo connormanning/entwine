a73061bd062a12abe8f1626212bf6da8ae0b2b99
Howard Butler <howard@hobu.co>
2020-10-06 13:33:23 -0500

    pin linux fixed build to pdal 2.2.0

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -9,7 +9,7 @@ conda install cmake ninja compilers -y
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
-    conda install --yes --quiet pdal gdal=3.0.2=py37hbb6b9fb_8 python=3.7 abseil-cpp  -y
+    conda install --yes --quiet pdal=2.2.0=h9463286_0 abseil-cpp  -y
     conda install --yes --quiet entwine  --only-deps -y
 
 else
