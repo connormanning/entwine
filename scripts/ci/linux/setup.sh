5421f78aec6a5991b4981c16ca170f0a38162b1c
Howard Butler <howard@hobu.co>
2020-11-17 10:13:55 -0600

    update pointer to 'fixed' linux build

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -9,7 +9,7 @@ conda install cmake ninja compilers -y
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
-    conda install --yes --quiet pdal=2.2.0=h9463286_0 abseil-cpp  -y
+    conda install --yes --quiet pdal=2.2.0=ha9435c1_1 abseil-cpp  -y
     conda install --yes --quiet entwine  --only-deps -y
 
 else
