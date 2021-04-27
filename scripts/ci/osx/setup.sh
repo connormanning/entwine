7e182a45970085470ccd3aebbcae2ec6905da4c1
Howard Butler <howard@hobu.co>
2021-04-27 10:57:30 -0500

    update fixed pins

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -9,7 +9,7 @@ conda install cmake ninja compilers -y
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
-    conda install --yes --quiet pdal gdal=3.0.4=py38h9f7df5a_9 python=3.8  -y
+    conda install --yes --quiet pdal gdal=3.2.2=py38hacca965_1 python=3.8 -y
     conda install --yes --quiet entwine  --only-deps -y
 
 else
