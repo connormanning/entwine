57da4a93fd741fad4a8bc0758d75454998b0c2f3
Howard Butler <hobu.inc@gmail.com>
2024-11-06 13:07:44 -0600

    churn conda

--- scripts/ci/conda/compile.sh
+++ scripts/ci/conda/compile.sh
@@ -2,23 +2,27 @@
 
 mkdir packages
 
-
-
 export CI_PLAT=""
 if grep -q "windows" <<< "$PDAL_PLATFORM"; then
     CI_PLAT="win"
+    ARCH="64"
 fi
 
 if grep -q "ubuntu" <<< "$PDAL_PLATFORM"; then
     CI_PLAT="linux"
+    ARCH="64"
 fi
 
 if grep -q "macos" <<< "$PDAL_PLATFORM"; then
     CI_PLAT="osx"
+    ARCH="arm64"
 fi
 
-conda mambabuild recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_.yaml"
+conda mambabuild recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_${ARCH}_.yaml"
+conda create -y -n test -c ./packages/${CI_PLAT}-${ARCH} entwine
+conda deactivate
 
-mamba install -c ./packages entwine
-
+conda activate test
 entwine --version
+conda deactivate
+
