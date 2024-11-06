ecb9e6c3b0ed61bc77097faade480f171c6e448a
Howard Butler <hobu.inc@gmail.com>
2024-11-06 13:49:52 -0600

    switch to setup-miniconda

--- scripts/ci/conda/compile.sh
+++ scripts/ci/conda/compile.sh
@@ -19,7 +19,7 @@ if grep -q "macos" <<< "$PDAL_PLATFORM"; then
 fi
 
 conda mambabuild recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_${ARCH}_.yaml"
-conda create -y -n test -c ./packages/${CI_PLAT}-${ARCH} entwine pdal
+conda create -y -n test -c ./packages/${CI_PLAT}-${ARCH} entwine
 conda deactivate
 
 conda activate test
