3dd6a748ce996cd5a894e9afdd99b92f8a10c819
Howard Butler <hobu.inc@gmail.com>
2025-04-04 10:49:59 -0500

    fetch nlohmann from remote if not available

--- cmake/nlohmann.cmake
+++ cmake/nlohmann.cmake
@@ -2,6 +2,27 @@
 # N Lohmann JSON handler
 #
 
+if(NOT CMAKE_REQUIRED_QUIET)
+  # CMake 3.17+ use CHECK_START/CHECK_PASS/CHECK_FAIL
+  message(STATUS "Looking for nlohmann")
+endif()
+find_package(nlohmann_json QUIET)
+set(USE_EXTERNAL_NLOHMANN_DEFAULT OFF)
+if(Nlohmann_FOUND)
+  if(NOT CMAKE_REQUIRED_QUIET)
+      message(STATUS "Looking for nlohmann - found (${Nlohmann_VERSION})")
+  endif()
+  if(Nlohmann_VERSION VERSION_LESS MIN_GTest_VERSION)
+    message(WARNING "External GTest version is too old")
+  else()
+    set(USE_EXTERNAL_NLOHMANN_DEFAULT ON)
+  endif()
+else()
+  if(NOT CMAKE_REQUIRED_QUIET)
+    message(STATUS "Looking for nlohmann - not found")
+  endif()
+endif()
+
 option(USE_EXTERNAL_NLOHMANN "Use an external nlohmann JSON library" OFF)
 
 if(USE_EXTERNAL_NLOHMANN)
