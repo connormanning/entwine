b7ba24a4d0575e733b3a44d0fcc0c9528e22418d
Isaac Bell <122644517+ibell13@users.noreply.github.com>
2025-06-18 14:09:29 -0500

    `resolve()`: pattern-match paths matching a postfix (#335)

--- entwine/util/fs.cpp
+++ entwine/util/fs.cpp
@@ -10,6 +10,8 @@
 
 #include <entwine/util/fs.hpp>
 
+#include <pdal/util/Utils.hpp>
+
 #include <stdexcept>
 
 namespace entwine
@@ -33,18 +35,28 @@ StringList resolve(const StringList& input, const arbiter::Arbiter& a)
     StringList output;
     for (std::string item : input)
     {
-        if (isDirectory(item))
+        if (isDirectory(item) || (item.find('*') != std::string::npos))
         {
+            // arbiter::resolve() globs for * or ** only if they are at the end of
+            // a path. To resolve these , we need to split the string
+            // so it ends in *, then filter the results later.
+            std::string postfix;
             const char last = item.back();
             if (last != '*')
             {
+                auto pos = item.find_last_of('*');
+                if (pos != std::string::npos)
+                {
+                    postfix = item.substr(pos + 1);
+                    item = item.substr(0, pos);
+                }
                 if (last != '/') item.push_back('/');
                 item.push_back('*');
             }
-
             const StringList directory(a.resolve(item));
             for (const auto& item : directory)
             {
+                if (postfix.size() && !pdal::Utils::endsWith(item, postfix)) continue;
                 if (!isDirectory(item)) output.push_back(item);
             }
         }
