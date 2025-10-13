089b67abf12c830aafa2ba614fdb9eefd837dbc2
Howard Butler <hobu.inc@gmail.com>
2025-10-13 09:55:38 -0500

    use rattler-build

--- scripts/ci/conda/compile.sh
+++ scripts/ci/conda/compile.sh
@@ -18,7 +18,7 @@ if grep -q "macos" <<< "$PDAL_PLATFORM"; then
     ARCH="arm64"
 fi
 
-conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_${ARCH}_.yaml"
+rattler-build build -r recipe --output-dir packages -m ".ci_support/${CI_PLAT}_${ARCH}_.yaml"
 conda create -y -n test -c ./packages/${CI_PLAT}-${ARCH} entwine
 conda deactivate
 
